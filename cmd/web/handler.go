c1ce3c6d9d1fbf814b5cd6b42c83a8ae0777a5f7
Emil <emilbabazade29@gmail.com>
2025-11-26 16:00:06 +0200

    better error handling

--- cmd/web/handler.go
+++ cmd/web/handler.go
@@ -9,15 +9,14 @@ import (
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	app.page(w, "./ui/html/pages/home.tmpl.html", nil)
+	app.page(w, r, "./ui/html/pages/home.tmpl.html", nil)
 }
 
 // view a todo
 func (app *application) getTodo(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.ParseInt(r.PathValue("id"), 10, 0)
 	if err != nil || id < 1 {
-		app.logger.Error("invalid pathvalue", "error", err)
-		http.NotFound(w, r)
+		app.clienError(w, http.StatusNotFound)
 		return
 	}
 	fmt.Fprintf(w, "Todo with id %d", id)
@@ -27,8 +26,7 @@ func (app *application) getTodo(w http.ResponseWriter, r *http.Request) {
 func (app *application) patchTodo(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.ParseInt(r.PathValue("id"), 10, 0)
 	if err != nil || id < 1 {
-		app.logger.Error("invalid pathvalue", "error", err)
-		http.NotFound(w, r)
+		app.clienError(w, http.StatusNotFound)
 		return
 	}
 	fmt.Fprintf(w, "Editing todo with id %d", id)
@@ -38,8 +36,7 @@ func (app *application) patchTodo(w http.ResponseWriter, r *http.Request) {
 func (app *application) deleteTodo(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.ParseInt(r.PathValue("id"), 10, 0)
 	if err != nil || id < 1 {
-		app.logger.Error("invalid pathvalue", "error", err)
-		http.NotFound(w, r)
+		app.clienError(w, http.StatusNotFound)
 		return
 	}
 	w.WriteHeader(http.StatusNoContent)
@@ -57,10 +54,10 @@ func (app *application) postTodoCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) notFound(w http.ResponseWriter, r *http.Request) {
-	app.page(w, "./ui/html/pages/notFound.tmpl.html", nil)
+	app.page(w, r, "./ui/html/pages/notFound.tmpl.html", nil)
 }
 
-func (app *application) page(w http.ResponseWriter, file string, data any) {
+func (app *application) page(w http.ResponseWriter, r *http.Request, file string, data any) {
 	files := []string{
 		"./ui/html/base.tmpl.html",
 		"./ui/html/partials/nav.tmpl.html",
@@ -69,15 +66,25 @@ func (app *application) page(w http.ResponseWriter, file string, data any) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.logger.Error("Invalid files", "error", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, r, err)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", data)
 	if err != nil {
-		app.logger.Error("Invalid templates", "error", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, r, err)
 		return
 	}
+}
+
+func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
+	method := r.Method
+	uri := r.URL.RequestURI()
+
+	app.logger.Error(err.Error(), "method", method, "uri", uri)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func (app *application) clienError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
 }
