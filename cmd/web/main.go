862d3fd7dc5f119efcd98cd16bb0f7ed2371d1c6
Emil <emilbabazade29@gmail.com>
2025-11-26 18:02:41 +0200

    db

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -13,6 +16,7 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", "127.0.0.1:5000", "HTTP network address")
+	dsn := flag.String("dsn", "webtodo:pass@/todos?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	logHandler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
@@ -21,14 +25,34 @@ func main() {
 	})
 	logger := slog.New(logHandler)
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
 	app := &application{
 		logger: logger,
 	}
 
-	app.logger.Info("Serving at", "addr", *addr)
-	err := http.ListenAndServe(*addr, app.routes())
+	logger.Info("Serving at", "addr", *addr)
+	err = http.ListenAndServe(*addr, app.routes())
+	logger.Error(err.Error())
+	os.Exit(1)
+}
+
+func openDB(dns string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dns)
 	if err != nil {
-		app.logger.Error(err.Error())
-		os.Exit(1)
+		return nil, err
 	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
